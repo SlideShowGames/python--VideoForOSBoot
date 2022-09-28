2ece760db8c5b7bf4046efe3bf81bbeb0dfc8ef2
Bradley Stratton <108354503+SlideShowGames@users.noreply.github.com>
2022-09-28 16:31:49 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#Thank you for adding Turtler boot video to your code if you want more sub packages go to
+# https://github.com/SlideShowGames/SlideShowGames and also check out my youtube https://www.youtube.com/channel/UCECRYbDpMr5aMyxhnS3ESGg
+import turtle
+def OSbooter():
+    turtle.TurtleScreen
+    turtle.color("blue")
+    turtle.bgcolor("Green")
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.color("white")
+    turtle.left(90)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.color("blue")
+    turtle.circle(100)
+    turtle.bgcolor("black")
+    turtle.bgpic("python-powered Turtler.gif")
+OSbooter()
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
